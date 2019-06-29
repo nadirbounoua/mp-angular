93785cb8429f9e27a23562908bce89df375435b1
Nadir Bounoua <anbounoua@gmail.com>
2019-06-29 16:37:47 +0100

    Added pie chart

--- frontend/src/app/analytics/analytics.component.ts
+++ frontend/src/app/analytics/analytics.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Chart } from 'chart.js'
 
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
   styleUrls: ['./analytics.component.scss'],
 })
-export class AnalyticsComponent implements OnInit {
+export class AnalyticsComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
+      let navbar = document.getElementsByTagName('app-navbar')[0].children[0];
+      navbar.classList.remove('navbar-transparent');  
   }
+
+	ngOnDestroy(){
+		let navbar = document.getElementsByTagName('app-navbar')[0].children[0];
+	}
 
 }
