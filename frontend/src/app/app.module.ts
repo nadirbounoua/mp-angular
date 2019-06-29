72313b09c8ea6a4a8d534399acb63c916f65f886
Aziz <a.attouche@esi-sba.dz>
2019-06-29 13:13:05 +0100

    create edit game form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { ExamplesModule } from './examples/examples.module';
+import {ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -24,12 +24,14 @@ import { EditgameComponent } from './editgame/editgame.component';
     imports: [
         BrowserAnimationsModule,
         NgbModule.forRoot(),
-        FormsModule,
         RouterModule,
         AppRoutingModule,
         ComponentsModule,
         ExamplesModule,
-        HttpClientModule
+        HttpClientModule,
+        ReactiveFormsModule,
+        FormsModule 
+    
     ],
     providers: [],
     bootstrap: [AppComponent]
--- frontend/src/app/editgame/editgame.component.ts
+++ frontend/src/app/editgame/editgame.component.ts
@@ -1,5 +1,6 @@
 import {  Component, OnInit, Inject, Renderer, ElementRef, OnDestroy } from '@angular/core';
 import { ApiService } from '../api.service';
+import { FormBuilder, FormGroup, FormControl,Validators,ValidationErrors} from '@angular/forms';
 
 @Component({
   selector: 'app-editgame',
@@ -8,14 +9,25 @@ import { ApiService } from '../api.service';
 })
 
 export class EditgameComponent implements OnInit, OnDestroy {
-
+  id: number;
   rows = [];
+  checkoutForm;
 
-  constructor( private element : ElementRef, private apiService : ApiService) { 
-       this.fetch((data) => {
+  constructor( private element : ElementRef, private apiService : ApiService, private formBuilder: FormBuilder) { 
+       /*this.fetch((data) => {
       this.rows = data;
       console.log(this.rows)
-        });
+        });*/
+
+        this.checkoutForm =  this.formBuilder.group({
+          status: ['' 
+                ],
+          platform: [''
+        ],
+          title: new FormControl(''),
+    
+          
+        });  
    }
 
   ngOnInit() {
@@ -28,13 +40,21 @@ export class EditgameComponent implements OnInit, OnDestroy {
 		let navbar = document.getElementsByTagName('app-navbar')[0].children[0];
 	}
 
-  fetch(cb) {
+  fetch(body,cb) {
       let magazines;
-        this.apiService.getUsers()
+        this.apiService.updateGame(1,body)
                        .subscribe((value) =>{
                        magazines = value;
                        cb(magazines);
                      });
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.fetch(customerData,(data) => console.log(data))
+    this.checkoutForm.reset();
   }
 
 }
